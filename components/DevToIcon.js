f0719418fdffdafac63d298b11b08caa6e868d52
Quoc Nguyen Trong <nguyentrongquocbk@gmail.com>
2025-02-23 21:05:10 +0700

    fix build

--- components/DevToIcon.js
+++ components/DevToIcon.js
@@ -1,9 +1,9 @@
 import Icon from "./Icon";
-import devIcon from '../assets/images/dev-to.svg'
+// import devIcon from '../assets/images/dev-to.svg'
 import { cls } from "../utils/template";
 
 const DevToIcon = ({ className, size }) => {
-  return <Icon src={devIcon} size={size} className={className} />
+  return <Icon src={""} size={size} className={className} />
 }
 
 export default DevToIcon;
